c7354b3ad756c26caf45707f9539afe72f18ed7f
Diego Moraes <diegofrost96@gmail.com>
2024-11-10 13:18:49 -0300

    add avatar component

---
+++ resources/views/components/ui/avatar.blade.php
@@ -0,0 +1,6 @@
+@props(['srcImg'])
+<div class="avatar">
+    <div class="w-8 h-8 rounded-full ring-primary ring-offset-base-100 ring ring-offset-2">
+        <img src="{{ $srcImg }}" />
+    </div>
+</div>
