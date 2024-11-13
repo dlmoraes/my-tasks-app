b50dbc83b3204198e0d0b56eb91bd7655b81e7db
Diego Moraes <diegofrost96@gmail.com>
2024-11-13 14:35:09 -0300

    add animation hover card and more informations

--- resources/views/components/ui/tasks/card.blade.php
+++ resources/views/components/ui/tasks/card.blade.php
@@ -1,18 +1,30 @@
-<li class="flex justify-between w-[18.75rem] items-center cursor-pointer">
-    <div class="card bg-base-100 w-full border border-dashed border-slate-700 shadow shadow-slate-700">
+@props(['task'])
+<li
+    class="card-task p-1 rounded-2xl group flex justify-between w-[18.75rem] h-[12rem]  items-center cursor-pointer hover:scale-105 transition-all duration-300 ease-linear">
+    <div class="w-full h-full border border-dashed shadow card bg-base-100 border-slate-700 shadow-slate-700">
         <div class="card-body">
             <h2 class="card-title">
-                <div class="badge badge-secondary">Serviço 1</div>
+                <div class="badge badge-secondary">{{ $task->service_name }}</div>
             </h2>
-            <p class="text-sm text-slate-400/90">If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <span class="flex gap-4 text-slate-500">
-                    1
-                    <i class="ri-attachment-line"></i>
-                </span>
-                <span class="flex gap-4 text-slate-500">
-                    <i class="ri-chat-1-line"></i>
-                </span>
+            <p class="text-sm text-slate-400/90">{{ $task->title }}</p>
+            <div class="card-actions">
+                <div class="flex items-center justify-between flex-1">
+                    <div class="flex items-center">
+                        <span class="text-xs">
+                            {{ $task->created_at->diffForHumans() }}
+                            <i class="ri-calendar-todo-line"></i>
+                        </span>
+                    </div>
+                    <div class="flex items-center justify-between gap-4">
+                        <span class="flex gap-1 text-slate-500">
+                            1
+                            <i class="ri-attachment-line"></i>
+                        </span>
+                        <span class="flex gap-1 text-slate-500">
+                            <i class="ri-chat-1-line"></i>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
