a40cc620995e51c447a842f0e4ae9037ebd38d0e
Diego Moraes <diegofrost96@gmail.com>
2024-11-13 14:30:45 -0300

    add simulate login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Services\ServiceService;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
 
     public function __invoke()
     {
+        if (!Auth::check()) {
+            Auth::loginUsingId(1);
+        }
+
         return view("home");
     }
 }
