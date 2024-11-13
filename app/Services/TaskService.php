103e799f39a5aba603d1d747adfd036644af1e1a
Diego Moraes <diegofrost96@gmail.com>
2024-11-13 14:33:23 -0300

    adjusts query eloquent task

---
+++ app/Services/TaskService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
+
+class TaskService
+{
+
+    function myTasks(int $authUserId)
+    {
+        return Task::join('services', 'tasks.service_id', '=', 'services.id')
+            ->select('tasks.id', 'tasks.title', 'tasks.description', 'tasks.status', 'tasks.created_at', 'services.name as service_name')
+            ->where('tasks.user_id', $authUserId)
+            ->orWhereExists(function (Builder $query) use ($authUserId) {
+                $query->select(DB::raw(1))
+                    ->from('services as s1')
+                    ->where('s1.user_id', $authUserId)
+                    ->whereColumn('s1.id', 'tasks.service_id');
+            })
+            ->get();
+    }
+}
