74f63589ba9cfa30c83acebee7733330778f2267
Diego Moraes <diegofrost96@gmail.com>
2024-11-13 14:31:54 -0300

    add link to task board route

--- resources/views/components/ui/header.blade.php
+++ resources/views/components/ui/header.blade.php
@@ -2,5 +2,12 @@
     <a href="{{ route('home') }}" class="text-2xl font-medium cursor-pointer">My<span class="font-bold">Tasks</span>
     </a>
 
+    <div class="flex items-center gap-6">
+        <a href="{{ route('task.index') }}" class="flex items-center gap-2 hover:font-semibold">
+            <i class="ri-kanban-view"></i>
+            Quadro de Tarefas
+        </a>
+    </div>
+
     <x-ui.avatar srcImg='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp' />
 </header>
