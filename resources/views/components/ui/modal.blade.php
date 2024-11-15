295da7c4bf24262bbc091ae9db86e3476286bbfa
Diego Moraes <diegofrost96@gmail.com>
2024-11-15 08:15:10 -0300

    adjust modal

--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -1,13 +1,8 @@
 <div x-data="{ showModal: @entangle('modal') }" x-show="showModal">
     <dialog x-ref="modal"
-        class="
-            fixed inset-0 rounded-lg w-full max-w-md
-            bg-[#181826] shadow-lg text-white
-            border-[#1E1E2C] border p-8
-            flex flex-col gap-6 z-50
-        ">
+        class="fixed inset-0 z-50 flex flex-col w-full max-w-md gap-6 p-8 text-white border rounded-lg shadow-lg">
         <div>
-            <button class="bg-[#1E1E2C] hover:bg-[#313145] transition duration-300 ease-in-out p-[8px] rounded-md"
+            <button class="btn btn-ghost transition duration-300 ease-in-out p-[8px] rounded-md"
                 wire:click="$set('modal', false)">
                 <i class="ri-close-line"></i>
             </button>
@@ -17,5 +12,5 @@
         </div>
     </dialog>
     <!-- Background Blur Overlay -->
-    <div x-show="showModal" class="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
+    <div x-show="showModal" class="fixed inset-0 z-40 bg-black bg-opacity-50 backdrop-blur-sm"></div>
 </div>
