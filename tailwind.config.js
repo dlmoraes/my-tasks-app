3a0c4a7387f9bca5a67a47020f9bad313e466896
Diego Moraes <diegofrost96@gmail.com>
2024-11-15 08:14:57 -0300

    adjusts themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,33 @@ export default {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    darkMode: "class",
     theme: {
         extend: {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                stratos: {
+                    50: "#e4efff",
+                    100: "#cfe1ff",
+                    200: "#a8c5ff",
+                    300: "#749eff",
+                    400: "#3e63ff",
+                    500: "#132bff",
+                    600: "#0013ff",
+                    700: "#0013ff",
+                    800: "#0011e4",
+                    900: "#0007b0",
+                    950: "#000154",
+                },
+            },
         },
         screens: {
-            xs: "576px",
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
         },
     },
     daisyui: {
